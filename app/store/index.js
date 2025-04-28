b22011ad7a93236a5e3e806727b584adeed2c95e
Muhammad-Ahsan-001 <drahsani001@gmail.com>
2025-04-28 22:54:20 +0500

    Update index.js

--- app/store/index.js
+++ app/store/index.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
 export default create((set) => ({
-    apiUrl: import.meta.env.NODE_ENV === 'production' ? 'https://something.com' : 'http://localhost:3000',
+    apiUrl: import.meta.env.NODE_ENV === 'production' ? 'http://193.203.191.142:5000' : 'http://localhost:3000',
     flowChart: { nodes: [], edges: [] },
     setFlowChart: (flowChart) => set({ flowChart }),
 }))
